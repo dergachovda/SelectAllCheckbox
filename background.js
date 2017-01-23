479c30a0a4b72e01cec70bc7a1a7e4e9e90ee6a8
Dima D <dergachovda@gmail.com>
2017-01-23 17:28:54 +0200

    added inversion select

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-    code: 'var nodes = document.getElementsByTagName("INPUT"); for (var i = 0; i < nodes.length; i++) { if (nodes[i].type == "checkbox") nodes[i].checked = true; }'
+    code: 'var nodes = document.getElementsByTagName("INPUT"); for (var i = 0; i < nodes.length; i++) { if (nodes[i].type == "checkbox") nodes[i].checked = !nodes[i].checked; }'
   });
 });
